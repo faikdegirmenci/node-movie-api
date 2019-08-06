c958d26b5b31437374eacf6ec7bc2feaeb53ec3b
Faik Değirmenci <faikdegirmenci@hotmail.com>
2019-08-06 15:22:52 +0300

    /movies/between endpoint

--- routes/movie.js
+++ routes/movie.js
@@ -2,14 +2,84 @@ const express = require('express');
 const router = express.Router();
 const Movie = require('../models/Movie');
 
-router.post('/movie', (req, res, next) => {
-
-  const movie = new Movie(req.body);
-
-  const promise = movie.save();
-
+router.get('/', (req, res, next) => {
+  const promise = Movie.find({});
   promise.then((data) => {
-    res.json({ status: 1 });
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+// top 10 list
+router.get('/top10', (req, res, next) => {
+  const promise = Movie.find({}).limit(10).sort({ imdb_score: -1 });
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+
+router.get('/:movie_id', (req, res, next) => {
+  //res.send(req.params); localhost:3000/api/movie/sfsasda yaptığımız zaman req.params ile sfsasda yı alabiliyoruz. 
+  const promise = Movie.findById(req.params.movie_id);
+  promise.then((movie) => {
+    if (!movie) {
+      next({ message: 'The movie was not found || veya 24 harfli bir sayı giriniz!!', code: 1 });
+    }
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.put('/:movie_id', (req, res, next) => {
+  //res.send(req.params); localhost:3000/api/movie/sfsasda yaptığımız zaman req.params ile sfsasda yı alabiliyoruz. 
+  const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, { new: true });
+  promise.then((movie) => {
+    if (!movie) {
+      next({ message: 'The movie was not found || veya 24 harfli bir sayı giriniz!!', code: 1 });
+    }
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.delete('/:movie_id', (req, res, next) => {
+  //res.send(req.params); localhost:3000/api/movie/sfsasda yaptığımız zaman req.params ile sfsasda yı alabiliyoruz. 
+  const promise = Movie.findByIdAndRemove(req.params.movie_id);
+  promise.then((movie) => {
+    if (!movie) {
+      next({ message: 'The movie was not found || veya 24 harfli bir sayı giriniz!!', code: 1 });
+    }
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.post('/', (req, res, next) => {
+  const movie = new Movie(req.body);
+  const promise = movie.save();
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+//Between
+router.get('/between/:start_year/:end_year', (req, res, next) => {
+  const { start_year,end_year } = req.params;
+  const promise = Movie.find(
+    { //$gte büyük ya da eşit demek. $lte küçük ya da eşit
+      year:{"$gte":parseInt(start_year),"$lte": parseInt(end_year)}
+    }
+  );
+  promise.then((data) => {
+    res.json(data);
   }).catch((err) => {
     res.json(err);
   });
